1e030ae821e3cc629b12b3380f024b364123cfcc
Artur Dani <darthur83@hotmail.com>
2023-09-22 15:22:47 +0200

    feat: add env configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { clientEnv, serverEnv } from "@/env"
+
 export default function Home() {
-  return <main className="mt-0 flex min-h-screen flex-col items-center justify-between pt-5"></main>
+  return (
+    <main className="mt-0 flex min-h-screen flex-col items-center justify-between pt-5">
+      client: [{clientEnv.NEXT_PUBLIC_PUBLISHABLE_KEY}] <br />
+      server: [{serverEnv.DATABASE_URL}] <br />
+    </main>
+  )
 }
