c674705c014e4902de76a6572900cf6e877670ed
Artur Dani <darthur83@hotmail.com>
2023-09-22 21:54:58 +0200

    feat: add shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button"
 import { clientEnv, serverEnv } from "@/env"
 
 export default function Home() {
   return (
-    <main className="mt-0 flex min-h-screen flex-col items-center justify-between pt-5">
+    <main className="mt-0 flex min-h-screen flex-col items-center justify-center gap-8 pt-5">
       client: [{clientEnv.NEXT_PUBLIC_PUBLISHABLE_KEY}] <br />
       server: [{serverEnv.DATABASE_URL}] <br />
+      <div>
+        <Button>Click me</Button>
+      </div>
     </main>
   )
 }
